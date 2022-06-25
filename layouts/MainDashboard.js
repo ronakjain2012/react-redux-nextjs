feba365a992234a1dfdc67fd64ddb894f0b31d5f
Ronak Bokaria <ronakjain2012@gmail.com>
2022-06-25 22:21:05 +0530

    design

--- layouts/MainDashboard.js
+++ layouts/MainDashboard.js
@@ -1,9 +1,8 @@
-
-import { Container, Grid } from '@mui/material';
+import { Divider, Grid, IconButton, InputBase, Paper } from '@mui/material';
 import React from 'react';
 import { connect } from 'react-redux';
 import styles from './styles/MainDashboard.module.css';
-
+import { ManageSearch } from '@mui/icons-material';
 class MainDashboard extends React.Component {
   constructor() {
     super();
@@ -12,10 +11,31 @@ class MainDashboard extends React.Component {
   render() {
     return (
       <Grid container className={styles.content}>
-        <Grid item md={2}>
-          as
+        <Grid item md={12}>
+          <Paper
+            component='form'
+            sx={{
+              p: '2px 4px',
+              display: 'flex',
+              alignItems: 'center',
+              borderRadius: 'var(--radius-1)',
+              marginBottom: '10px'
+            }}>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              fullWidth={true}
+              placeholder='Search'
+              inputProps={{ 'aria-label': 'search' }}
+            />
+            <IconButton type='submit' aria-label='search'>
+              <ManageSearch />
+            </IconButton>
+          </Paper>
         </Grid>
-        <Grid item md={2}> sa </Grid>
+        
+        <Grid item md={12}>
+        assasasa
+        </Grid>
       </Grid>
     );
   }
